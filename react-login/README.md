2b53e169f70ebd7b282bfbb0a26acdb4d44e7ae1
Fupin Yao <xcmax@qq.com>
2021-11-13 10:52:00 -0800

    d1

--- react-login/README.md
+++ react-login/README.md
@@ -1,3 +1,6 @@
+# Set up Https
+https://segmentfault.com/a/1190000037611649
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- react-login/src/App.js
+++ react-login/src/App.js
@@ -9,14 +9,26 @@ function App(){
   const [data, setData] = useState({});
   const [picture, setPicture] = useState('')
 
+  const responseFacebook = (response)=>{
+      console.log(response);
+      setData(response);
+      setPicture(response.picture.data.url);
+      if (response.accessToken){
+          setLogin(true)
+      }else{
+          setLogin(false)
+      }
+  }
   return (
-    <div class="container">
-      <Card>
-        <Card.Header>
+    <div className="container">
+      <Card style={{width:'800px'}} className="mx-auto mt-5">
+        <Card.Header className="pb-4">
           <h1>My React App</h1>
         </Card.Header>
         <Card.Body>
           <Card.Text>
+              {!login &&
+              <React.Fragment>
             <h3>Please ogin using one of the following</h3>
             <LoginForm/>
             <FacebookLogin appId="596762551567730" autoLoad={false} fields="name, email, picture" 
@@ -24,11 +36,27 @@ function App(){
               callback={responseFacebook}
               icon="fa-facebook"   
             />
+            </React.Fragment>
+            }
+            {login && <Home fbpic={picture} fbdata={data}/>}
           </Card.Text>
         </Card.Body>
       </Card>
     </div>
   )
+}
+
+function Home(fbpic, fbdata){
+    console.log(fbpic);
+    return (
+        <React.Fragment>
+            <img src={fbpic.fbpic} alt={fbdata.name}/>
+            <h3 className="d-inline text-success mx-2">
+                Welcome back {fbdata.name}
+            </h3>
+            <p className='my-5'> This is the home page of the app.</p>
+        </React.Fragment>
+    )
 }
 
 function LoginForm(){
@@ -43,3 +71,4 @@ function LoginForm(){
   )
 }
 
+export default App
